08e71d00f534e5b66ef8d547cd75a1d822dd8e00
Lars Rosengren <lars@larsro.se>
2025-10-29 21:37:05 +0900

    Update astro.config.ts
    Fix to site url routing

--- astro.config.ts
+++ astro.config.ts
@@ -36,8 +36,8 @@ import reading from "./src/utils/remark/reading";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://larsrose.github.io",
-  base: '/degrowthdesign',
+  site: "https://larsrose.github.io/degrowthdesign/",
+  base: '/degrowthdesign/',
   trailingSlash: "never",
   i18n: {
     locales: ["en", "zh-cn", "ja", "se"],
