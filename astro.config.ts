6aef2b0d81e123f8dc90302e84fe934a6c4b5403
Lars Rosengren <lars@larsro.se>
2025-10-29 18:20:07 +0900

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -36,7 +36,7 @@ import reading from "./src/utils/remark/reading";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thought-lite.vercel.app",
+  site: "https://larsrose.github.io/degrowthdesign/",
   trailingSlash: "never",
   i18n: {
     locales: ["en", "zh-cn", "ja", "se"],
